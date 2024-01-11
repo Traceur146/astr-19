73caf97bcdcbdf46e6b71ee57580c9c859530b1a
Traceur146 <156133510+Traceur146@users.noreply.github.com>
2024-01-10 16:02:11 -0800

    Session 1 prompt for Code Journal 1

---
+++ cj1-1.py
@@ -0,0 +1,8 @@
+#Linhang Sun Jan 10th 2024
+
+full_name = 'Linhang Sun'
+pronouns = 'he/him/his'
+movie = 'Iron Man'
+food = 'Noodle'
+
+print(f'My name is {full_name}. I use {pronouns} pronouns. My favorite movie is {movie}, and my favorite food is {food}.')
