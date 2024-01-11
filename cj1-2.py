6e3b2547a19a1c25df015aef8c29c9da59ca1ae0
Traceur146 <156133510+Traceur146@users.noreply.github.com>
2024-01-11 14:54:13 -0800

    Session 2 prompt for Journal Code 1.

---
+++ cj1-2.py
@@ -0,0 +1,11 @@
+f1 = 3.14
+f2 = 1.592
+i1 = 5
+i2 = 3
+
+print('3.14 + 1.592 =', f1 + f2)
+print(type(f1+f2))
+print('5 - 3 =', i1 - i2)
+print(type(i1 - i2))
+print('3.14 * 5 =', f1 * i1)
+print(type(f1 * i1))
